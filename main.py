b19453a995b4a099b9a4802482ab42b294dd7a03
Nithushan Mohan <nithushan014@gmail.com>
2023-08-03 04:12:40 -0700

    feat: text word changed

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ from xml.dom import HierarchyRequestErr
 import pyautogui
 import time
 
-pyautogui.alert('It will be started in 5ms after you click ok button')
+pyautogui.alert('It will be started in 3ms after you click ok button')
 
-time.sleep(5)
+time.sleep(3)
 
-text = 'Hi'
+text = 'Hello Every All'
 
 while True:
     pyautogui.typewrite(text)
-    time.sleep(2)
+    time.sleep(5)
     pyautogui.press('enter')
-
